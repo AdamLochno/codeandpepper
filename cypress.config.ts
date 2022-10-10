aedfaecaa78aad3df7308ddc433f77dfc91edcbf
Adam Lochno <adamlochno@Air-Adam.ht.home>
2022-10-10 20:44:55 +0200

    initialize repository

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    viewportWidth: 1440,
+    viewportHeight: 1080,
+    defaultCommandTimeout: 10000,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
